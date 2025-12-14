e1db8dd899bc9fd6c2168911f8042c7cf42dab73
quqiOnfree <quqiOnfree@outlook.com>
2025-12-15 01:09:38 +0800

    Add Cpp extension

---
+++ cpp_dll/src/main.cpp
@@ -0,0 +1,49 @@
+#include <pybind11/pybind11.h>
+#include <asio.hpp>
+#include <iostream>
+#include <sstream>
+
+#include "icmp_header.hpp"
+#include "ipv4_header.hpp"
+#include "ping.hpp"
+
+namespace py = pybind11;
+
+std::string ping(const std::string& dest, std::size_t count)
+{
+    asio::io_context io_context;
+    auto future = asio::co_spawn(io_context, async_ping(dest, count), asio::use_future);
+    io_context.run();
+    if (future.wait_for(std::chrono::nanoseconds(0)) == std::future_status::deferred)
+    {
+        return "Totally loss";
+    }
+    std::stringstream ss;
+    for (const auto& [ipv4_hdr, icmp_hdr, length, elapsed]: future.get())
+    {
+        ss << length - ipv4_hdr.header_length()
+        << " bytes from " << ipv4_hdr.source_address()
+        << ": icmp_seq=" << icmp_hdr.sequence_number()
+        << ", ttl=" << ipv4_hdr.time_to_live()
+        << ", time="
+        << chrono::duration_cast<chrono::milliseconds>(elapsed).count()
+        << "ms\n";
+    }
+    std::string data = ss.str();
+    if (data.empty()) {
+        return "Totally lost";
+    }
+    return data;
+}
+
+void f()
+{
+    std::cout << "hello world\n";
+}
+
+PYBIND11_MODULE(network_utils_externel_cpp, m)
+{
+    m.doc() = "a stupid function, print hello world";
+    m.def("hello", &f, "print hello world");
+    m.def("ping", &ping, "ping the destination");
+}
---
+++ src/plugins/networktools4qqbot/NetworkTools/test.py
@@ -0,0 +1,3 @@
+import network_utils_externel_cpp
+
+print(network_utils_externel_cpp.ping("www.baidu.com", 4))
